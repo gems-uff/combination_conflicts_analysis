cdfd6d1edeb69c963d403f92afb87b1154af5887
Heleno Campos <heleno_scj@hotmail.com>
2022-03-14 17:19:18 -0300

    Adds notebook for analyzing v1v2 and v2v1 cases

--- analysis_util.py
+++ analysis_util.py
@@ -14,15 +14,15 @@ def get_chunk_composition_boxplot(data, patterns, variable, variable_text, axis)
     sns.boxplot(x="group", y=variable, hue='partial_order', data=boxplot_data, showfliers = False, ax = axis).set(
     xlabel='Resolution pattern', ylabel=variable_text)
 
-def get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, start, end, step, last_delta):
+def get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, start, end, step, last_delta, variable):
     data = []
     for delta in range(start, end, step):
-        df_delta = df[(df['chunk_size_delta'] > last_delta) & (df['chunk_size_delta'] <= delta)]
+        df_delta = df[(df[variable] > last_delta) & (df[variable] <= delta)]
         if len(df_delta) > 0:
-            v1_c = v1_only[(v1_only['chunk_size_delta'] > last_delta) & (v1_only['chunk_size_delta'] <= delta)]
-            v2_c = v2_only[(v2_only['chunk_size_delta'] > last_delta) & (v2_only['chunk_size_delta'] <= delta)]
-            v1v2_c = v1v2[(v1v2['chunk_size_delta'] > last_delta) & (v1v2['chunk_size_delta'] <= delta)]
-            v2v1_c = v2v1[(v2v1['chunk_size_delta'] > last_delta) & (v2v1['chunk_size_delta'] <= delta)]
+            v1_c = v1_only[(v1_only[variable] > last_delta) & (v1_only[variable] <= delta)]
+            v2_c = v2_only[(v2_only[variable] > last_delta) & (v2_only[variable] <= delta)]
+            v1v2_c = v1v2[(v1v2[variable] > last_delta) & (v1v2[variable] <= delta)]
+            v2v1_c = v2v1[(v2v1[variable] > last_delta) & (v2v1[variable] <= delta)]
             others_c = len(df_delta) - len(v1_c) - len(v2_c) - len(v1v2_c) - len(v2v1_c)
             v1_p = len(v1_c)/len(df_delta)
             v2_p = len(v2_c)/len(df_delta)
@@ -33,13 +33,13 @@ def get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, start, end, step,
         last_delta = delta
     return data
 
-def get_strategies_plot_df(df, v1_only, v2_only, v1v2, v2v1):
-    delta_max = int(df['chunk_size_delta'].max())
-    delta_min = int(df['chunk_size_delta'].min())
+def get_strategies_plot_df(df, v1_only, v2_only, v1v2, v2v1, variable):
+    delta_max = int(df[variable].max())
+    delta_min = int(df[variable].min())
     data = []
-    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, delta_min, 0, 4, delta_min-1))
-    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, 0, 1, 1, -1))
-    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, 1, delta_max, 4, 0))
+    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, delta_min, 0, 4, delta_min-1, variable))
+    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, 0, 1, 1, -1, variable))
+    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, 1, delta_max, 4, 0, variable))
         
     return pd.DataFrame(data, columns=['delta', 'v1', 'v2', 'v1v2', 'v2v1', 'others'])
 
