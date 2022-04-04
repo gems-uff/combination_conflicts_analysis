4015d14e88471bd8c15d2c8c6ab8c81db19c47e9
Heleno Campos <heleno_scj@hotmail.com>
2022-04-04 14:37:18 -0300

    Fixes rq1 analysis notebook

--- analysis_util.py
+++ analysis_util.py
@@ -37,11 +37,17 @@ def get_strategies_plot_df(df, v1_only, v2_only, v1v2, v2v1, variable):
     delta_max = int(df[variable].max())
     delta_min = int(df[variable].min())
     data = []
-    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, delta_min, 0, 4, delta_min-1, variable))
-    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, 0, 1, 1, -1, variable))
-    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, 1, delta_max, 4, 0, variable))
+    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, delta_min, -10, 15, delta_min-1, variable))
+    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, -10, 10, 1, -1, variable))
+    data.extend(get_strategies_plot_data(df, v1_only, v2_only, v1v2, v2v1, 10, delta_max, 15, 0, variable))
         
-    return pd.DataFrame(data, columns=['delta', 'v1', 'v2', 'v1v2', 'v2v1', 'others'])
+    df = pd.DataFrame(data, columns=['delta', 'v1', 'v2', 'v1v2', 'v2v1', 'others'])
+    df['v1'] = df['v1'] * 100
+    df['v2'] = df['v2'] * 100
+    df['v1v2'] = df['v1v2'] * 100
+    df['v2v1'] = df['v2v1'] * 100
+    df['others'] = df['others'] * 100
+    return df
 
 def get_chunk_composition_pattern_data(pattern, data):
     return data[data['chunk_composition'] == f' {pattern}']
@@ -53,7 +59,7 @@ def get_normalized_composition_percentage_data(df):
     for i in np.arange(0.1, 0.6, 0.1):
         i = round(i, 1)
         last_i = round(last_i, 1)
-        range = f'({last_i}, {i}]'
+        range = f'[{last_i}, {i})'
         v1_count = len(df[(df['normalized_v1_percentage'] >= last_i) & (df['normalized_v1_percentage'] < i)])
         v2_count = len(df[(df['normalized_v2_percentage'] >= last_i) & (df['normalized_v2_percentage'] < i)])
         v1_data.append([v1_count, range])
@@ -88,3 +94,44 @@ def print_missing_lines(chunk_side, resolution):
             print(line)
         else:
             resolution.remove(line)
+
+'''
+    Returns a DataFrame containing our chunks dataset with all collected data
+    10,726 chunks resolved with combination
+    Removes 549 chunks that belong to projects that are implicit forks (potentially duplicated)
+    Total valid chunks: 10,177 
+'''
+def get_chunks_dataset():
+    df = pd.read_csv('data/chunks_info.csv')
+    df2 = pd.read_csv('data/partial_order_result.csv')
+    df = pd.merge(df, df2, on = ['chunk_id'])
+    df2 = pd.read_csv('data/resolution_composition.csv')
+    df = pd.merge(df, df2, on=['chunk_id'])
+    df['chunk_size'] = df['v1_size'] + df['v2_size']
+    print('All chunks: ', len(df))
+    df = filter_implicit_forks(df)
+    print('Valid chunks: ', len(df))
+    return df
+
+'''
+    Filter chunks from projects that are implicit forks
+'''
+def filter_implicit_forks(complete_dataset):
+    intersection = pd.read_csv('data/projects_intersection.csv') # borrowed from another study
+    all_chunks = pd.read_csv('data/all_chunks_ghiotto.csv')
+    df = all_chunks[all_chunks['chunk_id'].isin(list(complete_dataset['chunk_id']))]
+    selected_projects = df['project'].unique()
+    print('Total projects: ', len(selected_projects))
+    projects_that_intersect = intersection[intersection['intersection_perc'] > 0]
+    filtered_projects = set()
+    for index, row in projects_that_intersect.iterrows():
+        filtered_projects.add(row['project1'])
+        filtered_projects.add(row['project2'])
+    filtered_projects.remove('android/platform_frameworks_base') # we keep the biggest of the projects that intersect
+    print(f'Filtered {len(filtered_projects)} of {len(selected_projects)} projects for being implicit forks: {filtered_projects}')
+    selected_projects = set(selected_projects)
+    selected_projects = selected_projects - filtered_projects
+    print('Total valid projects: ', len(selected_projects))
+    df_without_forks = df[df['project'].isin(selected_projects)]
+    df_without_forks = complete_dataset[complete_dataset['chunk_id'].isin(df_without_forks['chunk_id'])]
+    return df_without_forks
